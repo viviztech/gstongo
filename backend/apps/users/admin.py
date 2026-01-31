024e42e9ba11ab2f2906595ae682fae5c3bcaa48
root <root@GEAR.>
2026-01-31 09:41:44 +0000

    Fix admin import, remove unfold UserAdmin

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -3,27 +3,30 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from unfold.admin import ModelAdmin, UserAdmin
 from .models import User, UserProfile, AdminProfile
 
+
 @admin.register(User)
 class UserAdmin(BaseUserAdmin, UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'phone_number', 'is_staff', 'is_active')
     search_fields = ('email', 'first_name', 'last_name', 'phone_number')
     ordering = ('email',)
-    
+
     # Custom fields for our User model
     fieldsets = BaseUserAdmin.fieldsets + (
         ('Verification Info', {'fields': ('phone_number', 'email_verified', 'phone_verified')}),
         ('Two-Factor Auth', {'fields': ('two_factor_enabled',)}),
     )
-    
+
     add_fieldsets = BaseUserAdmin.add_fieldsets + (
         ('Verification Info', {'fields': ('phone_number',)}),
     )
+
 
 @admin.register(UserProfile)
 class UserProfileAdmin(ModelAdmin):
     list_display = ('user', 'cin', 'gst_number', 'legal_name', 'city', 'state')
     search_fields = ('user__email', 'cin', 'gst_number', 'legal_name')
     list_filter = ('business_type', 'registration_type', 'state')
+
 
 @admin.register(AdminProfile)
 class AdminProfileAdmin(ModelAdmin):
